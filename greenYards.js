3d7cfd4fd412571c06e8ea55f0946954a4bd1dfd
Teodor Petrov <teodor.petrov@abv.bg>
2023-01-26 14:08:03 +0200

    new codes

---
+++ greenYards.js
@@ -0,0 +1,12 @@
+function greenYards (input) {
+
+    let squareMeters = (input[0]);
+    let price = squareMeters * 7.61;
+    let discount = 0.18 * price;
+    let finalPriceForTheProduct = price - discount;
+    
+    console.log(`The final price is: ${finalPriceForTheProduct} lv.`);
+    console.log(`The discount is: ${discount} lv.`)
+}
+
+greenYards (["550"])
---
+++ petShop.js
@@ -0,0 +1,9 @@
+function petShop(input) {
+    let petFood = (input[0]) * 2.50;
+    let catFood = (input[1]) * 4;
+    finalSum = petFood + catFood;
+
+    console.log(`${finalSum} lv.`);
+}
+
+petShop(["5 ", "4 "])
